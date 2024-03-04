ac49de68eb63a5d93f4a4ad44492a4a7c410b249
Klugah Daniel <klugahdaniel1@gmail.com>
2024-03-03 17:36:14 -0700

    migrations

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Defining the database entities
+        public DbSet<AppUser> Users { get; set; }
+        
+    }
+}
